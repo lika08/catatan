ffd344f53721c9ae489a0e1b19d338a441618ad7
unknown <likadwi08@gmail.com>
2021-04-15 01:10:54 +0700

    catat keuangan

--- app/src/main/java/com/example/keuangan/AwalActivity.java
+++ app/src/main/java/com/example/keuangan/AwalActivity.java
@@ -1,9 +1,19 @@
 package com.example.keuangan;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +22,38 @@ public class AwalActivity extends AppCompatActivity {
 
     private ViewPager screenPager;
     IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndacator;
+    Button btnNext;
+    int positon = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
 
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        //membuat layar full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
         setContentView(R.layout.activity_awal);
 
+        //HIDEN action bar
+        getSupportActionBar().hide();
+
+//        ini view
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        tabIndacator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.transition.button_animation);
+
         //fill list
-        List<ScreenItem> mList = new ArrayList<>();
+        final List<ScreenItem> mList = new ArrayList<>();
         mList.add(new ScreenItem("Save Money", "cobbbaaaaa dulu",R.drawable.img1));
         mList.add(new ScreenItem("Happy Your Life", "cobaa dluuuu yaaa",R.drawable.img2));
 
@@ -28,7 +62,66 @@ public class AwalActivity extends AppCompatActivity {
         introViewPagerAdapter = new IntroViewPagerAdapter(this,mList);
         screenPager.setAdapter(introViewPagerAdapter);
 
+//        setup tablayout dgn viewpage
+        tabIndacator.setupWithViewPager(screenPager);
+
+        //button next
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                positon = screenPager.getCurrentItem();
+                if (positon < mList.size()){
+
+                    positon++;
+                    screenPager.setCurrentItem(positon);
+                }
+
+                if(positon == mList.size()-1) {
+
+                    loaddLastScreen();
+                }
+            }
+        });
+
+        tabIndacator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == mList.size()-1){
+
+                    loaddLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivity = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
+            }
+        });
+
 
     }
 
+    private void loaddLastScreen(){
+
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndacator.setVisibility(View.INVISIBLE);
+
+        btnGetStarted.setAnimation(btnAnim);
+
+    }
 }
